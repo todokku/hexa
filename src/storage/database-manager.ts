4da3f1b637f57b4961b11814ca2f794859e5adfb
appasaheb4 <onlyappasaheb4@gmail.com>
2020-01-20 14:25:04 +0530

    app new icon.

--- src/storage/database-manager.ts
+++ src/storage/database-manager.ts
@@ -6,56 +6,57 @@ const initialize = () => db.init();
 const fetch = async key => {
   try {
     const data = await db.fetch();
-    if (data.rows._array.length === 0) {
+    if ( data.rows._array.length === 0 ) {
       return;
     }
-    const encryptedDatabase = data.rows._array[0].encData;
-    const database = decrypt(encryptedDatabase, key);
+    const encryptedDatabase = data.rows._array[ 0 ].encData;
+    const database = decrypt( encryptedDatabase, key );
 
     return database;
-  } catch (err) {
-    console.log(err);
+  } catch ( err ) {
+    console.log( err );
   }
 };
+
 
 const fetchSSS = async key => {
   try {
     const data = await db.fetchSSS();
-    if (data.rows._array.length === 0) {
+    if ( data.rows._array.length === 0 ) {
       return;
     }
-    const encryptedDatabase = data.rows._array[0].encData;
-    const database = decrypt(encryptedDatabase, key);
+    const encryptedDatabase = data.rows._array[ 0 ].encData;
+    const database = decrypt( encryptedDatabase, key );
 
     return database;
-  } catch (err) {
-    console.log(err);
+  } catch ( err ) {
+    console.log( err );
   }
 };
 
-const insert = async (database, key, inserted = true) => {
-  const encryptedDatabase = encrypt(database, key);
+const insert = async ( database, key, inserted = true ) => {
+  const encryptedDatabase = encrypt( database, key );
 
   try {
     inserted
-      ? await db.update(encryptedDatabase)
-      : await db.insert(encryptedDatabase);
-  } catch (err) {
-    console.log(err);
+      ? await db.update( encryptedDatabase )
+      : await db.insert( encryptedDatabase );
+  } catch ( err ) {
+    console.log( err );
     return false;
   }
   return true;
 };
 
-const insertSSS = async (database, key, inserted = true) => {
-  const encryptedDatabase = encrypt(database, key);
-  console.log({ encryptedDatabase });
+const insertSSS = async ( database, key, inserted = true ) => {
+  const encryptedDatabase = encrypt( database, key );
+  console.log( { encryptedDatabase } );
   try {
     inserted
-      ? await db.updateSSS(encryptedDatabase)
-      : await db.insertSSS(encryptedDatabase);
-  } catch (err) {
-    console.log(err);
+      ? await db.updateSSS( encryptedDatabase )
+      : await db.insertSSS( encryptedDatabase );
+  } catch ( err ) {
+    console.log( err );
     return false;
   }
   return true;
